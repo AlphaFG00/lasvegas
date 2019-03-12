8d2c85b0aaf9c6807d82db0270015de43130ce3a
AlphaFG00 <96fernando.guerrero@gmail.com>
2019-03-12 14:04:58 -0600

    no se que rayos

--- Daniel/En proceso/travelexpress-html-package/travelexpress/js/efecto.js
+++ Daniel/En proceso/travelexpress-html-package/travelexpress/js/efecto.js
@@ -1,26 +1,6 @@
 function mostrar(div)
  {
-/*<<<<<<< HEAD
 
-    document.getElementsByClassName("popular-pp-info").style.display = "block";
-     }
-function ocultar(){
-
-     document.getElementsByClassName("popular-pp-info").style.display = "none";
-}
-
-function dinamica(){
-    var clase = document.getElementsByClassName("popular-pp-info");
-        if(clase.style.display =="none"){
-
-            mostrar();
-            document.getElementsByClassName("btn-1 pp-btn").value = "Ocultar";
-
-        }
-        else{
-            ocultar(); 
-            document.getElementsByClassName("btn-1 pp-btn").value = "Ver Mas";
-=======*/
      document.getElementById(div).style.display = "block";
      }
 function ocultar(div){
